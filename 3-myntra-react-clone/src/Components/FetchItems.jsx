60d05acb4e0a26b6a6e8b7c3f78867aa1a11eff9
KishanWebDev <raik55488@gmail.com>
2024-08-08 13:21:18 +0530

    Changes added

--- 3-myntra-react-clone/src/Components/FetchItems.jsx
+++ 3-myntra-react-clone/src/Components/FetchItems.jsx
@@ -14,7 +14,7 @@ const FetchItems = () => {
     const signal = controller.signal;
     
     dispatch(fetchStatusActions.markFetchingStarted());
-    fetch("https://myntra-clone-1ol7.vercel.app/items", { signal })
+    fetch("http://localhost:8080/items", { signal })
       .then((res) => res.json())
       .then(({ items }) => {
         dispatch(fetchStatusActions.markFetchDone());
