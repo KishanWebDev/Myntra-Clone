4bbb477608dd3991c95dd9ef219a1c473659945a
KishanWebDev <raik55488@gmail.com>
2024-09-13 20:48:42 +0530

    change fetch url

--- 3-myntra-react-clone/src/Components/FetchItems.jsx
+++ 3-myntra-react-clone/src/Components/FetchItems.jsx
@@ -14,7 +14,7 @@ const FetchItems = () => {
     const signal = controller.signal;
     
     dispatch(fetchStatusActions.markFetchingStarted());
-    fetch("http://localhost:8080/items", { signal })
+    fetch("https://myntra-clone-4s0b.onrender.com/items", { signal })
       .then((res) => res.json())
       .then(({ items }) => {
         dispatch(fetchStatusActions.markFetchDone());
